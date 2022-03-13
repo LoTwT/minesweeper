2ff0596be426f34717c209795ed3f7d8857d5a59
LoTwT <709937065@qq.com>
2022-03-13 14:14:47 +0800

    chore: add prettier instead of eslint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'virtual:generated-pages'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createRouter, createWebHistory } from "vue-router"
+import routes from "virtual:generated-pages"
+import App from "./App.vue"
 
-import '@unocss/reset/tailwind.css'
-import './styles/main.css'
-import 'uno.css'
+import "@unocss/reset/tailwind.css"
+import "./styles/main.css"
+import "uno.css"
 
 const app = createApp(App)
 const router = createRouter({
@@ -13,4 +13,4 @@ const router = createRouter({
   routes,
 })
 app.use(router)
-app.mount('#app')
+app.mount("#app")
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,9 @@
-<script setup lang="ts">
-const name = $ref('')
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
+  <div>Minesweeper</div>
 
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
+  <div v-for="y in 10" :key="y">
+    <button v-for="x in 10" :key="x" w-5 h-5 border>{{ y * 10 + x }}</button>
   </div>
 </template>
